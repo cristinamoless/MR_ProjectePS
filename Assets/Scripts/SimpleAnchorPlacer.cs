ca1806c74c8961d0b3b8193585a18ae3fc02875e
luciacuriel <lucia.curiel@estudiantat.upc.edu>
2026-05-31 11:00:55 +0200

    no ha cambiado mucho pero asi lo teneis reciente

--- Assets/Scripts/SimpleAnchorPlacer.cs
+++ Assets/Scripts/SimpleAnchorPlacer.cs
@@ -4,17 +4,63 @@ using Meta.XR.BuildingBlocks;
 public class SimpleAnchorPlacer : MonoBehaviour
 {
     public SpatialAnchorSpawnerBuildingBlock spawner;
+    public GameObject previewPrefab;
+
+    private GameObject previewInstance;
+    private GameObject currentAnchor;
+
+    private bool canPlace = false;
+
+    void Start()
+    {
+        previewInstance = Instantiate(previewPrefab);
+        previewInstance.SetActive(false);
+    }
 
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.One)) // botón A
+        if (!canPlace)
         {
-            Transform cam = Camera.main.transform;
-
-            Vector3 position = cam.position + cam.forward * 1.5f;
-            Quaternion rotation = Quaternion.identity;
-
-            spawner.SpawnSpatialAnchor(position, rotation);
+            previewInstance.SetActive(false);
+            return;
         }
+
+        Transform cam = Camera.main.transform;
+        Vector3 pos = cam.position + cam.forward * 1.5f;
+
+        previewInstance.SetActive(true);
+        previewInstance.transform.position = pos;
+
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            PlaceAnchor(pos);
+        }
+    }
+
+    public void EnablePlacement()
+    {
+        canPlace = true;
+        previewInstance.SetActive(true);
+    }
+
+    public void DisablePlacement()
+    {
+        canPlace = false;
+        previewInstance.SetActive(false);
+    }
+
+    void PlaceAnchor(Vector3 position)
+    {
+        if (currentAnchor != null)
+        {
+            Destroy(currentAnchor);
+        }
+
+        spawner.SpawnSpatialAnchor(position, Quaternion.identity);
+
+        currentAnchor = GameObject.Find("Spatial Anchor(Clone)");
+
+        canPlace = false;
+        previewInstance.SetActive(false);
     }
 }
